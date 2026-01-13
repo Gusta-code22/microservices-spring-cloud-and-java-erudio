2faba6c3954bc89760f8e93ee8d638f0aa44425b
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-01-13 14:11:51 -0300

    Adicionando outra parte do projeto

---
+++ 08_MicroservicesWithFeignAndEureka/book-service/src/main/java/github/io/Gusta_code22/controller/BookController.java
@@ -0,0 +1,146 @@
+package github.io.Gusta_code22.controller;
+
+import github.io.Gusta_code22.dto.Exchange;
+import github.io.Gusta_code22.enviroment.InstanceInformationService;
+import github.io.Gusta_code22.model.Book;
+import github.io.Gusta_code22.proxy.ExchangeProxy;
+import github.io.Gusta_code22.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/book-service")
+public class BookController {
+
+    @Autowired
+    private InstanceInformationService informationService;
+
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private ExchangeProxy proxy;
+
+
+    @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Book findBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency){
+
+        String port = informationService.retrieveServerPort();
+
+
+        var book = repository.findById(id).orElseThrow();
+
+
+
+        Exchange exchange = proxy.getExchange(
+                book.getPrice(),
+                "USD",
+                currency);
+
+
+        book.setEnviroment(port + " FEIGN");
+        book.setPrice(exchange.getConvertedValue());
+        book.setCurrency(currency);
+        return book;
+    }
+
+//    @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
+//public Book findBook(
+//        @PathVariable("id") Long id,
+//        @PathVariable("currency") String currency){
+//
+//    String port = informationService.retrieveServerPort();
+//
+//
+//    var book = repository.findById(id).orElseThrow();
+//
+//    HashMap<String, String> params = new HashMap<>();
+//    params.put("amount", book.getPrice().toString());
+//    params.put("from", "USD");
+//    params.put("to", currency);
+//
+//    var response = new RestTemplate()
+//            .getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}",
+//                    Exchange.class, params);
+//
+//    Exchange exchange;
+//    exchange = response.getBody();
+//
+//    book.setEnviroment(port);
+//    book.setPrice(exchange.getConvertedValue());
+//    book.setCurrency(currency);
+//    return book;
+//}
+}
+
+//@GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
+//public Book findBook(
+//        @PathVariable("id") Long id,
+//        @PathVariable("currency") String currency){
+//
+//    String port = informationService.retrieveServerPort();
+//
+//
+//    var book = repository.findById(id).orElseThrow();
+//
+//    HashMap<String, String> params = new HashMap<>();
+//    params.put("amount", book.getPrice().toString());
+//    params.put("from", "USD");
+//    params.put("to", currency);
+//
+//    var response = new RestTemplate()
+//            .getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}",
+//                    Exchange.class, params);
+//
+//    Exchange exchange;
+//    exchange = response.getBody();
+//
+//    book.setEnviroment(port);
+//    book.setPrice(exchange.getConvertedValue());
+//    book.setCurrency(currency);
+//    return book;
+//}
+
+
+//*@GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
+//public Book findBook(
+//        @PathVariable("id") Long id,
+//        @PathVariable("currency") String currency){
+//
+//    String port = informationService.retrieveServerPort();
+//
+//
+//    var book = repository.findById(id).orElseThrow();
+//
+//    book.setEnviroment(port);
+//    book.setCurrency(currency);
+//    return book;
+//}
+
+// MOCKS
+//@GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
+//public Book findBook(
+//        @PathVariable("id") Long id,
+//        @PathVariable("currency") String currency){
+//
+//    String port = informationService.retrieveServerPort();
+//
+//    return new Book(
+//            1L,
+//            "Lobo branco",
+//            "The Wicther",
+//            new Date(),
+//            15.8,
+//            "BRL",
+//            port
+//    );
+//}
---
+++ 08_MicroservicesWithFeignAndEureka/exchange-service/src/main/java/github/io/Gusta_code22/model/Exchange.java
@@ -0,0 +1,95 @@
+package github.io.Gusta_code22.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "exchange")
+public class Exchange implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "from_currency", nullable = false, length = 3)
+    private String from;
+
+    @Column(name = "to_currency", nullable = false, length = 3)
+    private String to;
+
+    @Column(name = "conversion_factor")
+    private BigDecimal conversionFactor;
+
+    @Transient
+    private BigDecimal convertedValue;
+    @Transient
+    private String environment;
+
+    // Construtor padrão (Obrigatório para JPA)
+    public Exchange() {
+    }
+
+    // Construtor completo (Equivalente ao do Record)
+    public Exchange(Long id, String from, String to, BigDecimal conversionFactor,
+                    BigDecimal convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+}
