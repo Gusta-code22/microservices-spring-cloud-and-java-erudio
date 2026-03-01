6fabc4e9e90fad3e05f75c12c1956e56b1fbb67e
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-03-01 14:06:15 -0300

    Microservices Continuous Deployment with Github Actions

---
+++ 16_CIandCDwithGithubActionsandDockerHub/exchange-service/target/classes/db/migration/V2__Populate_Table_Exchange.sql
@@ -0,0 +1,8 @@
+INSERT INTO `exchange` (`from_currency`, `to_currency`, `conversion_factor`) VALUES
+                        ('USD', 'BRL', 5.42),
+                        ('USD', 'EUR', 0.85),
+                        ('USD', 'GBP', 0.74),
+                        ('USD', 'ARS', 1452.00),
+                        ('USD', 'CLP', 906.56),
+                        ('USD', 'COP', 3790.77),
+                        ('USD', 'MXN', 17.89);
