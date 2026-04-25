72e328af7718c839a86eb2f5c0fd7f1dc1586147
gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-04-25 17:23:04 -0300

    Initializing project on MacBook Air M4

---
+++ 19_MicroservicesRunningnKubernetesCluster/book-service/src/main/java/github/io/Gusta_code22/Startup.java
@@ -0,0 +1,16 @@
+package github.io.Gusta_code22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
